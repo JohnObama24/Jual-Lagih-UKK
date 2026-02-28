1af00886e05d1638c1cdafb0c845dc973ea5d10d
JohnObama24 <johnobama24keren@gmail.com>
2026-02-28 13:43:54 +0700

    feat: Implement cart management functionality and add an empty order controller.

---
+++ app/Http/Controllers/Cart/cartController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Cart;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\CartItem;
+use DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class cartController extends Controller
+{
+    //
+
+    public function addToCart($productId)
+    {
+
+        $user = Auth()->user();
+
+        $product = Product::findOrFail($productId);
+
+        DB::beginTransaction();
+
+        try {
+            $cart = Cart::firstOrCreate([
+                'user_id' => $user->id
+            ]);
+
+            $cartItem = CartItem::where([
+                'cart_id' => $cart->id,
+                'product_id' => $product->id
+            ])->first();
+
+            if ($cartItem) {
+                $cartItem->increment('quantity');
+            } else {
+                CartItem::create([
+                    'cart_id' => $cart->id,
+                    'product_id' => $product->id,
+                    'quantity' => 1
+                ]);
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return back()->with('error', 'Gagal menambahkan ke cart');
+        }
+
+    }
+
+
+
+    public function showCart()
+    {
+        $user = Auth()->user();
+
+        $cart = Cart::where('user_id', $user->id);
+
+        if (!cart) {
+            return redirect()->route('cart.index')->with('error', 'Cart not found');
+        }
+        return view('buyer.cart.index', compact('cart'));
+    }
+
+    public function updateQuantity(Request $request, $cartItemId)
+    {
+        $cartItem = CartItem::findOrFail($cartItemId);
+        $cartItem->update([
+            'quantity' => $request->quantity
+        ]);
+
+        return back();
+    }
+
+    public function remove($cartItemId)
+    {
+        CartItem::destroy($cartItemId);
+
+        return back();
+    }
+
+}
